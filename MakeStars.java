1666acd7096be70c623916310d095c3c95ba4fe6
Grace <gchen20@stuy.edu>
2021-01-04 13:14:27 -0500

    Initially tested and successfully printed stars instead of words

--- MakeStars.java
+++ MakeStars.java
@@ -4,11 +4,21 @@ import java.io.*;
 public class MakeStars {
 
   public static void main(String[]args) {
-
     Scanner in = new Scanner( System.in );
+
     while (in.hasNext()) {
       String line = in.nextLine();
-      System.out.println(line);
+      Scanner wordScan = new Scanner(line);
+
+      while (wordScan.hasNext()) {
+        String word = wordScan.next();
+        for (int i = 0; i < word.length(); i++) {
+          System.out.print("*");
+        }
+        System.out.print(" ");
+      }
+
+      System.out.println();
     }
 
   }
