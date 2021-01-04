cc2fc658bd149de822cffe083abafac08cd6cad0
Grace <gchen20@stuy.edu>
2021-01-04 13:01:20 -0500

    Coded so that the output echoes the input directly

---
+++ MakeStars.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+public class MakeStars {
+
+  public static void main(String[]args) {
+
+    Scanner in = new Scanner( System.in );
+    while (in.hasNext()) {
+      String line = in.nextLine();
+      System.out.println(line);
+    }
+
+  }
+
+}
