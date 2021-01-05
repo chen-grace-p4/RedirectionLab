e0fea038eadeba179654631ba39a2d8f5be3e40d
Grace <gchen20@stuy.edu>
2021-01-05 17:00:21 -0500

    Completed and tested Pig Latin II

--- PigLatin.java
+++ PigLatin.java
@@ -4,7 +4,6 @@ import java.io.*;
 public class PigLatin {
   public static String pigLatinSimple(String s) {
     String word = s;
-
     if (word.substring(0,1).equals("a") || word.substring(0,1).equals("e") || word.substring(0,1).equals("i") || word.substring(0,1).equals("o") || word.substring(0,1).equals("u")) {
       word += "hay";
     }
@@ -12,8 +11,22 @@ public class PigLatin {
       String first = word.substring(0,1);
       word = word.substring(1,word.length()) + first + "ay";
     }
-
     return word;
+  }
 
+  public static String pigLatin(String s){
+    String[] dictlist = {"bl", "br", "ch", "ck", "cl", "cr", "dr", "fl", "fr", "gh", "gl", "gr", "ng", "ph", "pl", "pr", "qu", "sc", "sh", "sk", "sl", "sm", "sn", "sp", "st", "sw", "th", "tr", "tw", "wh", "wr"};
+    List<String> dict = Arrays.asList(dictlist);
+
+    if (dict.contains(s.substring(0,2))) {
+      s = s.substring(2,s.length()) + s.substring(0,2) + "ay";
+    } else if (s.substring(0,1).equals("a") || s.substring(0,1).equals("e") || s.substring(0,1).equals("i") || s.substring(0,1).equals("o") || s.substring(0,1).equals("u")){
+      s += "hay";
+    } else {
+      String first = s.substring(0,1);
+      s = s.substring(1, s.length() ) + first + "ay";
+    }
+
+    return s;
   }
 }
